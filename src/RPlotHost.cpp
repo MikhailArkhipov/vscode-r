0b797a421b93e69c55a9ecb65ae496459a1b2628
MikhailArkhipov <mikhaila@outlook.com>
2015-11-11 14:53:01 -0800

    Improve plot window sizing

--- src/RPlotHost.cpp
+++ src/RPlotHost.cpp
@@ -1,24 +1,24 @@
 /* ****************************************************************************
- *
- * Copyright (c) Microsoft Corporation. All rights reserved. 
- *
- *
- * This file is part of Microsoft R Host.
- * 
- * Microsoft R Host is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Microsoft R Host is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
- *
- * ***************************************************************************/
+*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+*
+* This file is part of Microsoft R Host.
+*
+* Microsoft R Host is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Microsoft R Host is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ***************************************************************************/
 
 #include "stdafx.h"
 #include "RPlotHost.h"
@@ -27,13 +27,13 @@
 
 using namespace rplots;
 
-HWND RPlotHost::m_hwndPlotWindow = NULL;
+HWND RPlotHost::m_hwndToolWindow = NULL;
 HHOOK RPlotHost::m_hOldHook = NULL;
 bool RPlotHost::m_fProcessing = false;
 std::unique_ptr<RPlotHost> RPlotHost::m_pInstance = NULL;
 
-RPlotHost::RPlotHost(HWND wndPlotWindow) {
-    m_hwndPlotWindow = wndPlotWindow;
+RPlotHost::RPlotHost(HWND wndToolWindow) {
+    m_hwndToolWindow = wndToolWindow;
     m_hOldHook = ::SetWindowsHookEx(WH_CBT, CBTProc, NULL, ::GetCurrentThreadId());
 }
 
@@ -48,7 +48,7 @@ void RPlotHost::Terminate() {
         ::UnhookWindowsHookEx(m_hOldHook);
         m_hOldHook = NULL;
 
-    }    
+    }
     m_pInstance = NULL;
 }
 
@@ -60,27 +60,34 @@ LRESULT CALLBACK RPlotHost::CBTProc(
     if (nCode == HCBT_ACTIVATE) {
         if (!m_fProcessing) {
             m_fProcessing = true;
-            HWND hwnd = (HWND)wParam;
+            HWND hwndPlot = (HWND)wParam;
             WCHAR buf[100];
-            ::RealGetWindowClass(hwnd, buf, _countof(buf));
+            ::RealGetWindowClass(hwndPlot, buf, _countof(buf));
             if (wcscmp(buf, L"GraphApp") == 0) {
-                if (m_hwndPlotWindow != GetParent(hwnd)) {
-                    RECT rc;
-                    ::SetWindowLong(hwnd, GWL_STYLE, WS_CHILD);
-                    ::SetWindowLong(hwnd, GWL_EXSTYLE, 0);
-                    ::SetMenu(hwnd, NULL);
-                    ::SetWindowText(hwnd, NULL);
+                if (m_hwndToolWindow != GetParent(hwndPlot)) {
+                    RECT rcToolWindow, rcPlotWindow;
+                    ::SetWindowLong(hwndPlot, GWL_STYLE, WS_CHILD);
+                    ::SetWindowLong(hwndPlot, GWL_EXSTYLE, 0);
+                    ::SetMenu(hwndPlot, NULL);
+                    ::SetWindowText(hwndPlot, NULL);
 
-                    ::SetParent(hwnd, m_hwndPlotWindow);
-                    ::GetClientRect(m_hwndPlotWindow, &rc);
+                    ::GetClientRect(hwndPlot, &rcPlotWindow);
+                    ::GetClientRect(m_hwndToolWindow, &rcToolWindow);
 
-                    if (rc.right < 0) {
-                        rc.right = 200;
-                        rc.bottom = 300;
-                    }
+                    // Window has to be large enough to hold plot margins.
+                    // Plot window becomes child of the tool window and in IDE
+                    // user can size tool window to be very small. In this case
+                    // user may start getting errors like 'figure margin larger 
+                    // than the window'. We'll create window that is large enough
+                    // to hold the piicture even if it gets clipped in the IDE.
+                    rcToolWindow.right = max(rcToolWindow.right, rcPlotWindow.right);
+                    rcToolWindow.bottom = max(rcToolWindow.bottom, rcPlotWindow.bottom);
 
-                    ::SetWindowPos(hwnd, HWND_TOP, 0, 0, rc.right, rc.bottom, SWP_SHOWWINDOW | SWP_FRAMECHANGED);
-                    ::PostMessage(m_hwndPlotWindow, WM_ACTIVATE_PLOT, 0, 0);
+                    ::SetParent(hwndPlot, m_hwndToolWindow);
+
+                    // Resize tool window to make sure it fits plot window
+                    ::SetWindowPos(m_hwndToolWindow, HWND_TOP, 0, 0, rcToolWindow.right, rcToolWindow.bottom, SWP_SHOWWINDOW | SWP_FRAMECHANGED);
+                    ::PostMessage(m_hwndToolWindow, WM_ACTIVATE_PLOT, 0, 0);
                 }
             }
             m_fProcessing = false;
