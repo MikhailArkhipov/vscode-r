f4dd56052491815a319cc835dcb4a1f424784bd4
Mikhail Arkhipov <mikhaila@microsoft.com>
2015-11-17 19:51:32 -0800

    Encoding conversion via CP_ACP

--- src/util.cpp
+++ src/util.cpp
@@ -1,24 +1,24 @@
 /* ****************************************************************************
- *
- * Copyright (c) Microsoft Corporation. All rights reserved. 
- *
- *
- * This file is part of Microsoft R Host.
- * 
- * Microsoft R Host is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Microsoft R Host is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
- *
- * ***************************************************************************/
+*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+*
+* This file is part of Microsoft R Host.
+*
+* Microsoft R Host is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Microsoft R Host is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ***************************************************************************/
 
 #include "stdafx.h"
 #include "util.h"
@@ -27,8 +27,15 @@ namespace po = boost::program_options;
 
 namespace rhost {
     namespace util {
+        std::locale single_byle_locale() {
+            boost::locale::generator gen;
+            gen.use_ansi_encoding(); // Windows must use locale for non-Unicode programs (CP_ACP)
+            return gen.generate("");
+        }
+
         std::string to_utf8(const char* buf, size_t len) {
-            auto& codecvt_wchar = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale());
+            std::locale loc = single_byle_locale();
+            auto& codecvt_wchar = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(loc);
             std::wstring_convert<std::codecvt<wchar_t, char, std::mbstate_t>> convert(&codecvt_wchar);
             auto ws = convert.from_bytes(buf, buf + len);
 
@@ -40,7 +47,8 @@ namespace rhost {
             std::wstring_convert<std::codecvt_utf8<wchar_t>> codecvt_utf8;
             auto ws = codecvt_utf8.from_bytes(u8s);
 
-            auto& codecvt_wchar = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(std::locale());
+            std::locale loc = single_byle_locale();
+            auto& codecvt_wchar = std::use_facet<std::codecvt<wchar_t, char, std::mbstate_t>>(loc);
             std::wstring_convert<std::codecvt<wchar_t, char, std::mbstate_t>> convert(&codecvt_wchar);
             return convert.to_bytes(ws);
         }
@@ -60,14 +68,17 @@ namespace boost {
                 if (colon == s.npos) {
                     host = s;
                     port = 5118;
-                } else {
+                }
+                else {
                     host = s.substr(0, colon);
                     auto port_str = s.substr(colon + 1);
                     try {
                         port = std::stoi(port_str);
-                    } catch (std::invalid_argument&) {
+                    }
+                    catch (std::invalid_argument&) {
                         throw po::validation_error(po::validation_error::invalid_option_value);
-                    } catch (std::out_of_range&) {
+                    }
+                    catch (std::out_of_range&) {
                         throw po::validation_error(po::validation_error::invalid_option_value);
                     }
                 }
@@ -75,7 +86,8 @@ namespace boost {
                 boost::asio::ip::address address;
                 try {
                     address = boost::asio::ip::address::from_string(host);
-                } catch (boost::system::system_error&) {
+                }
+                catch (boost::system::system_error&) {
                     throw po::validation_error(po::validation_error::invalid_option_value);
                 }
 
--- src/util.h
+++ src/util.h
@@ -1,24 +1,24 @@
 /* ****************************************************************************
- *
- * Copyright (c) Microsoft Corporation. All rights reserved. 
- *
- *
- * This file is part of Microsoft R Host.
- * 
- * Microsoft R Host is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * Microsoft R Host is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
- *
- * ***************************************************************************/
+*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+*
+*
+* This file is part of Microsoft R Host.
+*
+* Microsoft R Host is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Microsoft R Host is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Microsoft R Host.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ***************************************************************************/
 
 #pragma once
 #include "stdafx.h"
@@ -56,7 +56,8 @@ namespace rhost {
                 if (_p) {
                     try {
                         (*_p)();
-                    } catch (...) {
+                    }
+                    catch (...) {
                         std::terminate();
                     }
                 }
@@ -83,7 +84,6 @@ namespace rhost {
 
         typedef std::unique_ptr<SEXP, SEXP_delete> unique_sexp;
 
-
         std::string to_utf8(const char* buf, size_t len);
 
         inline std::string to_utf8(const std::string& s) {
@@ -95,7 +95,7 @@ namespace rhost {
         }
 
         std::string from_utf8(const std::string& u8s);
-
+        std::locale single_byle_locale();
 
         template<class Arg>
         inline void append(picojson::array& msg, Arg&& arg) {
@@ -117,7 +117,8 @@ namespace rhost {
         inline auto exceptions_to_errors(F f) -> decltype(f()) {
             try {
                 return f();
-            } catch (std::exception& ex) {
+            }
+            catch (std::exception& ex) {
                 Rf_error(ex.what());
             }
         }
