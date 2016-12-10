ad243201f17ed0e80347e27e97da9f9b689fe1de
MikhailArkhipov <mikhaila@microsoft.com>
2016-12-09 19:09:54 -0800

    data() support in remote (silent fetch_file)

--- src/r_util.cpp
+++ src/r_util.cpp
@@ -502,14 +502,14 @@ namespace rhost {
             return Rf_ScalarInteger(static_cast<int>(lock_state));
         }
 
-        extern "C" SEXP fetch_file(SEXP path) {
+        extern "C" SEXP fetch_file(SEXP path, SEXP silent) {
             const char* f_path = Rf_translateCharUTF8(STRING_ELT(path, 0));
             return util::exceptions_to_errors([&]() {
                 fs::path fpath = fs::u8path(f_path);
                 if (!fpath.empty()) {
                     blobs::blob file_data;
                     blobs::append_from_file(file_data, fpath);
-                    host::send_notification("!FetchFile", file_data, fpath.filename().string());
+                    host::send_notification("!FetchFile", file_data, fpath.filename().string(), Rf_asLogical(silent) != 0 ? true : false);
                     return R_TrueValue;
                 }
                 return R_FalseValue;
